949357cfbc2af7a6e85313dc2a3653f582de7cdc
Andrin Meier <andrin.meier@hispeed.ch>
2021-12-16 12:38:43 +0100

    add a class for creating a lightning show
    We could use this for the last CG maze before creating a real maze

---
+++ LightningShow.js
@@ -0,0 +1,6 @@
+export class LightningShow {
+    activate(scene) {
+        // The scene is what we are applying the lightning show on.
+        // Now activate multiple lights and move the lights, redrawing the scene each time.
+    }
+}
